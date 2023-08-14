7bf4e37d40c6a3f542684bd9b5fa4a899dc0319e
tj705 <sujins0311@naver.com>
2023-08-14 19:18:56 +0900

    first work

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MainController {
+	
+	
+	@GetMapping({"","/"})
+	public ResponseEntity<?> index() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("id", "k002");
+		
+		return ResponseEntity.ok().body(map);
+	}
+	
+	@GetMapping({"/health"})
+	public ResponseEntity<?> health() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("status", "ok");
+		
+		return ResponseEntity.ok().body(map);
+	}
+
+}
---
+++ src/main/java/com/example/demo/MavenSample02Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenSample02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenSample02Application.class, args);
+	}
+
+}
